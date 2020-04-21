303defc6513189a30a8fe3b19b4e1373cb512a3a
Rangga-PR <ranggaputraraharja@gmail.com>
2020-04-21 21:02:23 +0700

    add window resize event listener to search section

--- src/components/searchdomainsection/SearchDomainSection.vue
+++ src/components/searchdomainsection/SearchDomainSection.vue
@@ -7,7 +7,8 @@
           <Input placeholder="Ketik nama domain" size="medium" />
         </div>
         <div class="column is-one-fifth-desktop is-3-mobile">
-          <Button variant="red" stretch icon="search" />
+          <Button v-if="onMobile" variant="red" stretch icon="search" />
+          <Button v-else variant="red" text="Cari Domain" stretch />
         </div>
       </div>
     </div>
@@ -26,6 +27,28 @@ export default {
   },
   props: {
     title: String,
+  },
+  data() {
+    return {
+      onMobile: false,
+    };
+  },
+  methods: {
+    checkIfOnMobile: function() {
+      const deviceWidth = window.innerWidth;
+      if (deviceWidth <= 768) this.onMobile = true;
+      else this.onMobile = false;
+    },
+    onWindowSizeChange: function(event) {
+      this.checkIfOnMobile();
+    },
+  },
+  mounted() {
+    window.addEventListener("resize", this.onWindowSizeChange);
+    this.checkIfOnMobile();
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onWindowSizeChange);
   },
 };
 </script>
